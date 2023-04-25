bb25d733ddc4d493ad47891e2ba2d9390766f016
Дмитрий Каличкин <dmitrijkalickin@Dmitrijs-Laptop.local>
2023-04-25 10:02:18 +0700

    Users without gateway done

--- users-service/src/main/java/com/bank/users/AuthApplication.java
+++ users-service/src/main/java/com/bank/users/AuthApplication.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Users Service",
         description = "This service provides endpoints related to users in bank system",
         version = "2.0"))
+@EnableDiscoveryClient
 public class AuthApplication {
 
     public static void main(String[] args) {
